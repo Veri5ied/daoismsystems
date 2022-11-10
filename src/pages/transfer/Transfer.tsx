ef4031c0f3ca40e0e9b1e741c69d37e7ce045ab7
Veri5ied <alvinuchenna14@gmail.com>
2022-11-10 09:50:14 +0100

    mint and wallet

--- src/pages/transfer/Transfer.tsx
+++ src/pages/transfer/Transfer.tsx
@@ -1,7 +1,28 @@
+import ConnectButton from "../../components/buttons/ConnectButton";
+
 const Transfer = () => {
   return (
     <div>
       <h1>Transfer</h1>
+      <div className="general-component">
+        <div className="general-component__form">
+          <div className="general-component__form__input">
+            <label htmlFor="recipient">Recipient</label>
+            <input type="text" id="recipient" />
+          </div>
+          <div className="general-component__form__input">
+            <label htmlFor="amount">Amount</label>
+            <input type="text" id="amount" />
+          </div>
+          <div className="general-component__form__input">
+            <ConnectButton
+              buttonText="Send Token"
+              disabled={false}
+              onClick={() => console.log("clicked")}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
